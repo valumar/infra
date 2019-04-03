7dd8187abf5843d0bf43ff57c273e493a459b730
Petr E. Antonov <antpeter@gmail.com>
2019-04-03 18:30:57 +0300

    Modifying the proces of Ruby install

--- start_script.sh
+++ start_script.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-RUBY_VERSION='2.4.1'
-
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-rvm requirements
-rvm install $RUBY_VERSION
-rvm use $RUBY_VERSION --default
-gem install bundler -V --no-ri --no-rdoc
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
 ruby -v
 bundle -v
 
