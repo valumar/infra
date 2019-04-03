82e73bb70c34022af986fc075875cd4b03f7615f
Petr E. Antonov <antpeter@gmail.com>
2019-04-03 12:11:40 +0300

    Fix issues with deploying

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MONGODB_VER="3.2"
+MONGODB_VER='3.2'
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/$MONGODB_VER multiverse" > /etc/apt/sources.list.d/mongodb-org-$MONGODB_VER.list'
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/$MONGODB_VER multiverse" > /etc/apt/sources.list.d/mongodb-org-$MONGODB_VER.list
 sudo apt update; sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
