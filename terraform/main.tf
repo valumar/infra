b5bb11ae39c8fa1fbbc7351f90a2b192944d9547
Petr E. Antonov <antpeter@gmail.com>
2019-04-06 13:30:42 +0300

    Adding firewall rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ resource "google_compute_instance" "app" {
     name = "reddit-app"
     machine_type = "f1-micro"
     zone = "us-west1-b" 
+    tags = ["reddit-app"]
     metadata {
         sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
     }
@@ -24,4 +25,19 @@ resource "google_compute_instance" "app" {
         # использовать ephemeral IP для доступа из Интернет
         access_config {}
     }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+    name = "allow-puma-default"
+    # Название сети, в которой действует правило
+    network = "default"
+    # Какой доступ разрешить
+    allow {
+        protocol = "tcp"
+        ports = ["9292"]
+    }
+    # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
+    # Правило применимо для инстансов с тегом …
+    target_tags = ["reddit-app"]
 }
